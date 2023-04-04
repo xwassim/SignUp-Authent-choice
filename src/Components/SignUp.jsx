e57094a6361ede3eab6039904cdd4c6f96cf38d5
Cheref wassim <95647708+xwassim@users.noreply.github.com>
2023-04-04 17:27:21 +0100

    Add files via upload

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import Form from './Form';
-import Navbar from './Navbar'
+import React from "react";
+import Form from "./Form";
+import Navbar from "./Navbar";
 function SignUp() {
   return (
     <>
-    <nav>
-    <Navbar/>
-    </nav>
-    <div className='titre'><h1 className='text-4xl'>Inscrivez vous !</h1></div>
-    <div className='formulaire'>
-    <Form/>
-   </div>
-   </>
+      <nav>
+        <Navbar />
+      </nav>
+      <div className="titre">
+        <h1 className="text-4xl">Inscrivez vous !</h1>
+      </div>
+      <div className="formulaire">
+        <Form />
+      </div>
+    </>
   );
 }
 
